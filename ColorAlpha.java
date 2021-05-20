a0c3a5094bb6c6e80c83c84dc554c7fed218a6c1
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2021-05-20 12:59:17 +0200

    fixes

--- ColorAlpha.java
+++ ColorAlpha.java
@@ -6,13 +6,8 @@ public class ColorAlpha {
     public final Integer A;
     public final Color color;
 
-
     public ColorAlpha(Integer R, Integer G, Integer B, Integer A) {
-        this.R = R;
-        this.G = G;
-        this.B = B;
-        this.A = A;
-        this.color = new Color(R, G, B);
+        this(new Color(R, G, B), A);
     }
 
     public ColorAlpha(Color color, Integer A) {
@@ -21,7 +16,8 @@ public class ColorAlpha {
             String error;
             error = "A should be between 0 and 255, got " + A + " instead.";
             throw new IllegalArgumentException(error);
-          }
+        }
+        this.color = color;
         this.R = color.R;
         this.G = color.G;
         this.B = color.B;
@@ -36,10 +32,10 @@ public class ColorAlpha {
         if (hexString.length() != 9) {
             throw new IllegalArgumentException("invalid : hexString should have a length of 9.");
         }
-        Color color = new Color(hexString.substring(0, 7));
-        this.R = color.R;
-        this.G = color.G;
-        this.B = color.B;
+        this.color = new Color(hexString.substring(0, 7));
+        this.R = this.color.R;
+        this.G = this.color.G;
+        this.B = this.color.B;
         hexString = hexString.toLowerCase();
         for (int i=6; i <= 8; i++) {
             if ("0123456789abcdef".indexOf(hexString.charAt(i)) == -1) {
