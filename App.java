e7dfcf1bbd96697f096f9df12d28ca3ce88fbcbc
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2021-05-24 12:11:29 +0200

    update

--- App.java
+++ App.java
@@ -14,24 +14,44 @@ import java.util.List;
 
 public class App {
   public static void main(String[] args) {
-    SVGAttributes svgAttributes = new SVGAttributes();
-    svgAttributes.setXMLNS("http://www.w3.org/2000/svg");
-    svgAttributes.setWidth("300");
-    svgAttributes.setHeight("200");
-    svgAttributes.setViewBox("0 0 300 200");
+    String[][] svgAttributesArray = {
+      {"xmlns", "http://www.w3.org/2000/svg"},
+      {"version", "1.1"},
+      {"baseProfile", "full"},
+      {"width", "300"},
+      {"height", "200"},
+      {"viewbox", "0 0 300 200"}
+    };
+    SVGAttributes svgAttributes = new SVGAttributes(svgAttributesArray);
 
-    // fuck, consider the Rect. The API verbosity is unbelievably shitty.
-    // Lets do it anyway.
-    Geometry geometry = new Geometry();
-    geometry.setWidth("100");
-    geometry.setHeight("100");
-    Radii radii = new Radii();
-    Fill fill = new Fill();
-    fill.setColor("red");
-    Stroke stroke = new Stroke();
+    String[][] rectangleAttributesArray = {
+      {"width", "100%"},
+      {"height", "100%"},
+      {"fill", "red"}
+    };
+    RectangleAttributes rectangleAttributes = new RectangleAttributes(rectangleAttributesArray);
 
-    SVG svg = new SVG(svgAttributes,
-        new Rectangle(geometry, radii, fill, stroke)
+    String[][] circleAttributesArray = {
+      {"cx", "150"},
+      {"cy", "100"},
+      {"r", "80"},
+      {"fill", "green"}
+    };
+    CircleAttributes circleAttributes = new CircleAttributes(circleAttributesArray);
+
+    String[][] textAttributesArray = {
+      {"x", "150"},
+      {"y", "125"},
+      {"font-size", "60"},
+      {"text-anchor", "middle"},
+      {"fill", "white"}
+    };
+    TextAttributes textAttributes = new TextAttributes(textAttributesArray);
+
+ SVG svg = new SVG(svgAttributes,
+        new Rectangle(rectangleAttributes),
+        new Circle(circleAttributes),
+        new Text(textAttributes, new TextNode("SVG"))
     );
 
     System.out.println(svg.toXML());
